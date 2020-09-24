d8008a392a0b366dfeebdbdcccda8e2cb70f4de1
Ziauddin Robin <robin.cse.sust@gmail.com>
2020-09-24 09:04:55 +0600

    htaccess updated # about page, 404 page, form submission js updated. added validation from both php and javascript

---
+++ lib/email.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once 'validation/Validation/Validation.php';
+include_once 'sendEmail.php';
+
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$data = json_decode($json);
+
+//print_r($data);
+
+// validate the data and send it to administrator into a email
+
+ $val = new Validation();
+
+ $val->name('email')->value($data->email)->pattern('email')->required();
+ $val->name('name')->value($data->name)->pattern('words')->required();
+ $val->name('message')->value($data->message)->pattern('text')->required();
+ 
+ if($val->isSuccess()){
+     $response = send_email($data->name, $data->email, $data->message);
+ }else{
+    $response = array('status' => 0, 'message' => $val->getErrors());
+ }
+ header("Content-typ: application/json");
+ echo json_encode($response);
+ exit;
+
+?>
