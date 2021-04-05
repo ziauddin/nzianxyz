1e4a6f57ecf9ca9dad64068163916677e9c7baa1
ziauddin <robin.cse.sust@gmail.com>
2021-04-06 01:37:49 +0600

    add validation and update email.php accordingly

--- lib/email.php
+++ lib/email.php
@@ -3,7 +3,8 @@ error_reporting(0);
 ini_set('display_errors', 0);
 
 
-include_once 'validation/Validation/Validation.php';
+include_once 'validation/validation.php';
+
 include_once 'sendEmail.php';
 
 // Takes raw data from the request
