a896e1807f519b7336977f9a799ad253c78a94b6
Ziauddin Robin <robin.cse.sust@gmail.com>
2023-01-11 23:06:53 +0600

    added new page content and pages. soon deploy in the server

---
+++ assets/js/data.js
@@ -0,0 +1,9 @@
+const siteInfo = {
+  name: "Nzian",
+  developer: "devnzian",
+  baseUrl: {
+    development: "https://www.dev.nzianxyz",
+    production: "https://www.nzian.xyz",
+  },
+  mode: "development",
+};
